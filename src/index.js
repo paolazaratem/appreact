85eb85054cf0f0b1f1e78d79611b01658fba474e
Paola Zarate GeekGirl <paolazaratem@gmail.com>
2017-06-06 12:10:56 +0200

    add component: Comment, Avatar and UserInfo comments

--- src/index.js
+++ src/index.js
@@ -41,6 +41,13 @@ function Avatar(props) {
 
 function UserInfo(props){
   return(
+    /*
+    Extracting components might seem like grunt work at first, but having a palette of reusable
+    components pays off in larger apps. A good rule of thumb is that if a part of your UI is used
+    several times (Button, Panel, Avatar), or is complex enough on its own (App, FeedStory, Comment),
+    it is a good candidate to be a reusable component.
+    */
+
     <div className = "UserInfo">
     {/*
       add the information in comment, but is better render avatar in other Component
